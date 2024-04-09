894a79821cfaceed2c07ea353c0380df91881054
wcamer <xeus.xiii@hotmail.com>
2024-04-08 23:38:28 -0600

    clean up commented out code

--- src/public/js/news.js
+++ src/public/js/news.js
@@ -1,63 +1,3 @@
-/*
-Needed components
-    news
-
-*/
-
-// const url = 'https://myallies-breaking-news-v1.p.rapidapi.com/GetTopNews';
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Key': 'e3276b0b90msh316c195a1c4399cp17a7fajsnaa73fb904586',
-// 		'X-RapidAPI-Host': 'myallies-breaking-news-v1.p.rapidapi.com'
-// 	}
-// };
-
-// try {
-// 	const response = await fetch(url, options);
-// 	const result = await response.text();
-// 	console.log(result);
-// } catch (error) {
-// 	console.error(error);
-// }
-// 'use strict';
-// var request = require('request');
-
-// // replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
-// var url = 'https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers=AAPL&apikey=demo';
-
-// request.get({
-//     url: url,
-//     json: true,
-//     headers: {'User-Agent': 'request'}
-//   }, (err, res, data) => {
-//     if (err) {
-//       console.log('Error:', err);
-//     } else if (res.statusCode !== 200) {
-//       console.log('Status:', res.statusCode);
-//     } else {
-//       // data is successfully parsed as a JSON object:
-//       console.log(data);
-//     }
-//   });
-
-
-// const options = {method: 'GET', headers: {accept: 'application/json'}};
-
-// fetch('https://api.webz.io/filterWebContent?q=%28%22stock%20market%22%20OR%20NASDAQ%29%20language%3Aenglish&format=json&sort=crawled&ts=1595156935835&highlight=true&token=ce20533f-49b2-401a-a4cb-b3ecb8d72f86', options)
-//   .then(response => response.json())
-//   .then(response => console.log(response))
-//   .catch(err => console.error(err));
-
-
-
-
-
-
-
-
-
-
 import { loadHeaderFooter } from "/src/public/js/utilities.mjs"; ///js/utilities.mjs
 //src\public\js\utilities.js
 
@@ -67,34 +7,7 @@ var url = 'https://newsapi.org/v2/everything?' +
           'q=stocks&' +
           'apiKey=e4e21500877041bb915a5f7f49646946';
 
-// let page = 1; // this is the default page where it starts
-// createStory(page, articles)
 
-
-// var req = new Request(url);
-
-// fetch(req)
-//     .then( async function(response) {
-//         console.log(response)
-//         const data =  await response.json();
-//         const art = data.articles //length is 100
-//         console.log("------------------------",data,"**********************",art[0].description,"\n",art.length)
-
-
-
-//     })
-
-//let pageButton = document.querySelectorAll(".pageTurnerButton") // returns a nodelist of the children
-// console.log(pageButton)
-// for(let i = 0; i < pageButton.length; i++){
-//     pageButton[i].addEventListener("click", pressed())
-//     console.log(pageButton[i].innerHTML)
-//     console.log("a;sldkj;falks\n")
-// }
-
-
-
-//////////////////////////////////////////////
 //these are the page turner buttons
 let p1 = document.querySelector("#ptb1")
 let p2 = document.querySelector("#ptb2")
@@ -127,96 +40,7 @@ await fetch(newsReq)
 
 
     })
-/*
-    daily open/close
-        close
-        open
-    TICKERS
-        ticker
-        name
 
-*/
-
-////Need ticker, name, current value 
-////this will get the 'ticker' and 'name'
-
-
-// let tickersUrl = "https://api.polygon.io/v3/reference/tickers?active=true&apiKey=b7VVurJtA8Yn_G0_61TUsj0wJynF3w0y"
-// let tickers
-// //this needs to get the date and ticker name
-// let dailyOCUrl = "https://api.polygon.io/v1/open-close/{ticker}/{date here}?adjusted=true&apiKey=b7VVurJtA8Yn_G0_61TUsj0wJynF3w0y"
-
-// await fetch(tickersUrl)
-//     .then(async function (response) {
-//         console.log("here is the tickerURL response.....\n",response)
-//         const tickerReqData = await response.json();
-//         tickers = tickerReqData
-//        ///////////so i don't want to make multipole requests because it will burn through my requests for the day
-//        //probably will only do name and tickers
-
-//     })
-
-
-
-// console.log("here is tickers outside the request\n",tickers)
-
-// async function stockStream(){
-//     console.log("////////////////////////////////////")
-//     let results = tickers.results
-//     console.log("*****************************************\n",results)
-
-//     let divStream = document.querySelector("#stockStream")
-//     divStream.setAttribute("id","stockStream")
-
-//     //await fetch()
-
-//     for(let i = 0; i < results.length; i++){
-        
-
-//         let divItem = document.createElement("div")
-//         divItem.classList.add("stockStreamItem")
-
-//         let divItemDetails = document.createElement("div")
-//         divItemDetails.classList.add("stockStreamItemDetails")
-//         divItemDetails.setAttribute("id",`stockStreamItemDetails${results[i].ticker}`)
-
-//         let ticker = document.createElement("p")
-//         ticker.innerHTML = results[i].ticker
-
-//         let tn = document.createElement("p")
-//         tn.innerHTML = results[i].name
-
-//         let addButton = document.createElement("button")
-//         addButton.innerHTML = "Add to watchlist"
-//         addButton.addEventListener("click", () => {
-//                                                                     //when it's ".children" it returns a list
-//             let see = document.querySelector(`#stockStreamItemDetails${results[i].ticker}`).children
-//             console.log("this ticker has been added to localstorage",see)
-//         })
-
-//         divItemDetails.appendChild(ticker)
-//         // divItemDetails.appendChild(tn)
-
-//         divItem.appendChild(divItemDetails)
-//         divItem.appendChild(addButton)
-
-//         divStream.append(divItem)
-//         // let headerSection = document.querySelector("#headerSection")
-//         // headerSection.append(divStream)
-        
-//     };
-
-   
-    
-
-
-
-// }
-
-// stockStream()
-
-//let art = await data.articles
-// console.log("------------------------\n",data,"\n**********************\n",art[0].description,"\n",art.length)
 console.log("------------------------\n",data,)
 let articles = data.articles
 let page = 1; // this is the default page where it starts
@@ -350,7 +174,7 @@ function removeStories() {
     allStories.forEach(story => {
         story.remove()
     })
-    //allStories.remove()
+    
 }
 
 function buttonReset(){
@@ -367,21 +191,12 @@ function buttonReset(){
 
 ////////////////////
 
-let article = document.createElement('article')//single story container
-let section = document.createElement('section')
-let img = document.createElement('img')//holds story image
-let desc = document.createElement('p')// description under title
-let title = document.createElement('h2') // title under image
-let link = document.createElement('a') // link to more if you want to know more
-console.log("hello from new.js")
+// let article = document.createElement('article')//single story container
+// let section = document.createElement('section')
+// let img = document.createElement('img')//holds story image
+// let desc = document.createElement('p')// description under title
+// let title = document.createElement('h2') // title under image
+// let link = document.createElement('a') // link to more if you want to know more
+// console.log("hello from new.js")
 
 
-// // componetns ="https://api.polygon.io + daily open/close + stockTicker + adjusted (this is optional) + apikey"
-// let stockUrl ="https://api.polygon.io/v1/open-close/AAPL/2024-03-06?adjusted=true&apiKey=b7VVurJtA8Yn_G0_61TUsj0wJynF3w0y"
-
-// let req1 = new Request(stockUrl)
-// fetch(req1)
-//     .then(async function (response){
-//         const data = await response.json()
-//         console.log("/*/*/*/*/*/*8/**'\n",data)
-//     })
